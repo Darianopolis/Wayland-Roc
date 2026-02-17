881afb6824ce2d9863af90010f5fdd208db2a567
Darianopolis <darianopolis@gmail.com>
2026-02-17 05:02:46 +0000

    [wren] Add wren_const_ptr alias to allow const buffer access in shaders

--- src/wren/shaders/shared.h
+++ src/wren/shaders/shared.h
@@ -6,6 +6,9 @@
 
 using image4f32 = wren_image_handle<vec4f32>;
 
+template<typename T>
+using wren_const_ptr = const T*;
+
 #else
 #include "shared.slang"
 #endif
--- src/wroc/shaders/imgui.h
+++ src/wroc/shaders/imgui.h
@@ -2,9 +2,13 @@
 
 #include "wren/shaders/shared.h"
 
+#ifndef WREN_SHADER
+struct ImDrawVert;
+#endif
+
 struct wroc_imgui_shader_in
 {
-    ImDrawVert* vertices;
+    wren_const_ptr<ImDrawVert> vertices;
     vec2f32 scale;
     vec2f32 offset;
     image4f32 texture;
